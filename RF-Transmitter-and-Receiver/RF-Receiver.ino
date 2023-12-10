12ed3b35f29c5cd05212e90494af630cb911a81a
shameermohamed <shameer50@gmail.com>
2023-12-10 13:52:04 +0530

    Create RF-Receiver.ino

---
+++ RF-Transmitter-and-Receiver/RF-Receiver.ino
@@ -0,0 +1,35 @@
+//Tech Trends Shameer
+//RF Transmitter and Receiver
+
+//Below is the Receiver Program
+
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+// Include dependant SPI Library 
+#include <SPI.h> 
+
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+
+void setup()
+{
+    // Initialize ASK Object
+    rf_driver.init();
+    // Setup Serial Monitor
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // Set buffer to size of expected message
+    uint8_t buf[24];
+    uint8_t buflen = sizeof(buf);
+    // Check if received packet is correct size
+    if (rf_driver.recv(buf, &buflen))
+    {
+      
+      // Message received with valid checksum
+      Serial.print("Message Received: ");
+      Serial.println((char*)buf);         
+    }
+}
